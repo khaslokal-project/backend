8041021035c45cca5aa4ea74e9a03ea080d61ef2
Indra Putra <indrakoslab@gmail.com>
2018-08-21 14:14:31 +0700

    method put for adminCourier

--- controller/admincourier.js
+++ controller/admincourier.js
@@ -34,7 +34,39 @@ const admincourierController = {
       .catch(error => {
         res.status(400).send(error)
       })
+  },
+
+  // edit admin curier data
+  update: (req, res, next)=> {
+    const id = Number(req.params.id)
+    if(req.body.addressseller){
+      Admincourier.update({
+        addressseller: req.body.addressseller,
+        nameproduct: req.body.nameproduct,
+        quantity: req.body.quantity,
+        priceproduct: req.body.priceproduct,
+        totalproductprice: req.body.totalproductprice,
+        tariff: req.body.tariff,
+        grandtotal: req.body.grandtotal,
+        iduser: req.body.iduser,
+        addressuser: req.body.addressuser,
+        phone: req.body.phone,
+        updatedAt: new Date()
+      }, {
+        where: { id: id }
+      })
+      .then(()=> {
+        res.status(200).send({
+          message: 'Admin Courier updated'
+        })
+      })
+    } else {
+      res.status(416).send({
+        message: 'Please specify password and email!'
+      })
+    }
   }
+
 }
 
 module.exports = admincourierController
