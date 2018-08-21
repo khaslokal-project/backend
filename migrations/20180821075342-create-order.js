347e714bcb5f89608e331f9a13310f7eb8e36410
Indra Putra <indrakoslab@gmail.com>
2018-08-21 15:14:59 +0700

    dump mysql db

---
+++ migrations/20180821075342-create-order.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      idproduct: {
+        type: Sequelize.INTEGER
+      },
+      iduser: {
+        type: Sequelize.INTEGER
+      },
+      idcourier: {
+        type: Sequelize.INTEGER
+      },
+      idseller: {
+        type: Sequelize.INTEGER
+      },
+      total: {
+        type: Sequelize.BIGINT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Orders');
+  }
+};
