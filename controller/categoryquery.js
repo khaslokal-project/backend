fa3808193a144ca7985555246eafe0f7e95e827f
Indra Putra <indrakoslab@gmail.com>
2018-08-21 13:08:49 +0700

    merge conflicts

--- controller/categoryquery.js
+++ controller/categoryquery.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-const { Products } = require("../models");
-
-const categoryController = {
-  // search
-  searchByCategory: (req, res) => {
-    Products.findAll({
-      where: {
-        idcategory: 1
-      }
-    }).then(result => {
-      console.log(result);
-      if (result) {
-        res.status(200).send({
-          result
-        });
-      } else {
-        res.status(404).send({
-          message: "No data"
-        });
-      }
-    });
-=======
 const { Products, productCategory } = require("../models");
 // const sequelize = require("sequelize");
 
@@ -34,7 +11,6 @@ const categoryController = {
         .catch(error=> {
           res.status(500).send(error)
         })
->>>>>>> 37ff9bb3df6adaa25f625d40a5934328fa868c0b
   }
 };
 
