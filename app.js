4f134aa6f82a4cc3b80f936e09230212e81222b9
Indra Putra <indrakoslab@gmail.com>
2018-08-21 10:51:12 +0700

    add cors configuration

--- app.js
+++ app.js
@@ -24,6 +24,12 @@ app.use(cookieParser());
 
 app.use(cors());
 
+app.use((req, res, next)=> {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/admins", adminsRouter);
