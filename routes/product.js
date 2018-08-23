9e355ad5921fe5dcdc6507a3f0b4b89111f9f404
Indra Putra <indrakoslab@gmail.com>
2018-08-23 14:41:29 +0700

    update route and controller

--- routes/product.js
+++ routes/product.js
@@ -3,18 +3,21 @@ var router = express.Router();
 
 const productController = require("../controller/product");
 
-router.post("/", productController.add);
-
 router.get("/", productController.get);
 
+// searh products by id
 router.get("/:id", productController.search);
+
+router.post("/", productController.add);
 
 router.put("/:id", productController.update);
 
 router.delete("/:id", productController.remove);
 
+// search product by keyword
 router.post("/search/:name", productController.searchByKeyword);
 
+// show product sort by productCategories table
 router.get("/category/:idcategory", productController.searchByCategory);
 
 module.exports = router;
